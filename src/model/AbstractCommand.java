af2de4ced04d921ea6d6f1697800af84be0ec082
elkie <lkieabcde@localhost>
2020-06-04 17:32:27 +1000

    Undo refine

--- src/model/AbstractCommand.java
+++ src/model/AbstractCommand.java
@@ -1,14 +1,14 @@
-//package model;
-//
-//abstract class AbstractCommand {
-//
-//    abstract Command execute();
-//
-//	void execute(MovablePiece movablePiece) {
-//		
-//		
-//	}
-//
-//
-//
-//}
+package model;
+
+abstract class AbstractCommand {
+
+    abstract void execute();
+
+	void execute(MovablePiece movablePiece) {
+		
+		
+	}
+
+
+
+}
